584b90223f53b7b53afd695bdd0ba153d84e41b8
VIVEK VERMA <119332108+verma2209@users.noreply.github.com>
2024-09-22 22:51:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// "Shop Now in navbar" button click alert
+document.querySelector(".shop-now").addEventListener("click", function () {
+  alert("Redirecting to Shop Page!");
+});
+// "Shop Now in middle" button click alert
+document.querySelector(".shop-now-hero").addEventListener("click", function () {
+  alert("Redirecting to Shop Page!");
+});
+
+// Function to show alert when an item is added to cart
+function addToCart() {
+  alert("Item added to cart!");
+}
